7e5c6522ad1791f615b7a4c4f6467526f3587a68
Mark Laukkanen <mark.laukkanen@gmail.com>
2016-02-28 18:29:31 +0200

    Added all the files

--- Hide The Bomb/ViewController.swift
+++ Hide The Bomb/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var blueBomb: UIImageView!
+    @IBOutlet weak var redBomb: UIImageView!
+    @IBOutlet weak var blueButton: UIButton!
+    @IBOutlet weak var redButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +25,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func hideBlueBomb(sender: AnyObject) {
+        blueBomb.hidden = true
+    }
+    
+    @IBAction func hideRedBomb(sender: AnyObject) {
+        redBomb.hidden = true
+    }
+    
+    
 
 }
 
